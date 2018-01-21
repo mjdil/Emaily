c9e51fa05a16bdfa930be0324d9448dcf783c1bb
Muhammad Jamal <muhammadjamal@Muhammads-MacBook-Pro.local>
2018-01-20 22:04:03 -0500

    changed greeting

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
 const app = express();
-//the / after get means that when they enter that it will show whatever is in the function
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
-});
+
+
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clinetSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken)=> {
+  console.log(accessToken);
+}));
+//the strings are not random google has a list of strings that you can ask for
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+
+
+
 //this tells us which port to listen to
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
