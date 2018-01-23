37a67df34bf8b5aee10640253ff5065842f2d673
Muhammad Jamal <muhammadjamal@Muhammads-MacBook-Pro.local>
2018-01-23 09:06:41 -0500

    tweak google strat

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '238740428404-bljj28m5gpvbmdsrkokqkuh6dlku47e5.apps.googleusercontent.com',
+  googleClientSecret: 'eY70C30LS7ei8xfKjmUEUgrr',
+  mongoURI: 'mongodb://<adil>:<muhammad10>@ds111258.mlab.com:11258/emaily-dev-adil',
+  cookiekey: 'dhfjsdhfuburisgbhksbufibbfuidsbuofhndwuonwekffj'
+};
